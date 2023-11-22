4e035950b32afd1924d55399ca042f4b4ccae7ae
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2023-11-22 16:30:45 +0800

    New Task now tracks changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
-  const tasks =  [
-    { name: 'Go Shopping', id: 1},
-    { name: 'Clean the Car', id: 2},
-    { name: 'Read book', id: 3},
-  ];
-  const listTasks = tasks.map(task =>
-    <li key={task.id}> {task.name} </li>
-  );
 
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setCount((count) => count + 1);
-  }
+  const [message, setMessage] = useState('');
+ 
+  const handleChange = event => {
+    setMessage(event.target.value);
+    console.log('value is:', event.target.value);
+  };
 
 
   return (
@@ -27,22 +20,23 @@ function App() {
 
        <div className="listbox">
           <h4> Current Tasks </h4>
-          <ul className="currentTask">{listTasks}</ul>
-          {count}
+          <div className="currentTasks">
+            here
+          </div>
        </div>
 
        <div className="listbox">
        <h4> New Task </h4>
           <div className="newTask">
 
-          <form >
-          <input type="text" onChange={(e) => setInput(e.target.value)}></input>
-          <button type="submit" onClick={handleSubmit}>
-            submit
-          </button>
-          </form>
+          <input
+            type="text"
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={message}>
+            </input>
           
-        
           </div>
        </div>
      

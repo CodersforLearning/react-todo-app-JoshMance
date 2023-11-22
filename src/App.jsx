98c0db2bc77bab6f2edd9e88a8aa322e9586ddf0
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2023-11-22 14:54:21 +0800

    Creating submit form

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const nextId = 4;
   const tasks =  [
     { name: 'Go Shopping', id: 1},
     { name: 'Clean the Car', id: 2},
@@ -14,29 +13,40 @@ function App() {
     <li key={task.id}> {task.name} </li>
   );
 
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCount((count) => count + 1);
+  }
+
+
   return (
     <div className="App">
        <h1>Josh's Task Planner</h1>
+       <hr />
 
        <div className="listbox">
           <h4> Current Tasks </h4>
           <ul className="currentTask">{listTasks}</ul>
+          {count}
        </div>
 
        <div className="listbox">
        <h4> New Task </h4>
           <div className="newTask">
 
-            <input type="text" id="taskNames" name="name" required minlength="4" maxlength="8" size="20" />
+          <form >
+          <input type="text" onChange={(e) => setInput(e.target.value)}></input>
+          <button type="submit" onClick={handleSubmit}>
+            submit
+          </button>
+          </form>
           
+        
           </div>
        </div>
      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+
     </div>
   )
 }
